691f04bf80acb3c9e0ae3db646e1de5f1874b559
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2017-08-29 04:13:14 +1000

    Get skeletal animation working

--- tutorial.js
+++ tutorial.js
@@ -4,10 +4,8 @@ var animationSystem = require('skeletal-animation-system')
 var mat4ToDualQuat = require('mat4-to-dual-quat')
 
 var baseballPlayer = require('./cowboy.json')
-console.log(baseballPlayer)
 baseballPlayer = expandVertexData(baseballPlayer)
 baseballPlayer.keyframes = require('./cowboy.json').keyframes
-console.log(baseballPlayer)
 
 var canvas = document.createElement('canvas')
 canvas.width = 500
@@ -22,7 +20,6 @@ gl.enable(gl.DEPTH_TEST)
 var vertexGLSL = `
 attribute vec3 aVertexPosition;
 attribute vec3 aVertexNormal;
-attribute vec2 aWeight;
 
 attribute vec4 aJointIndex;
 attribute vec4 aJointWeight;
@@ -134,6 +131,8 @@ void main (void) {
 
   // We only have one index right now... so the weight is always 1.
   gl_Position = leftHandedPosition;
+
+  vLightWeighting = vec3(1.0, 1.0, 1.0);
 }
 `
 
@@ -166,15 +165,13 @@ gl.linkProgram(shaderProgram)
 gl.useProgram(shaderProgram)
 
 var vertexPosAttrib = gl.getAttribLocation(shaderProgram, 'aVertexPosition')
-gl.enableVertexAttribArray(vertexPosAttrib)
-
 var vertexNormalAttrib = gl.getAttribLocation(shaderProgram, 'aVertexNormal')
-gl.enableVertexAttribArray(vertexNormalAttrib)
-
 var jointIndexAttrib = gl.getAttribLocation(shaderProgram, 'aJointIndex')
-gl.enableVertexAttribArray(jointIndexAttrib)
-
 var jointWeightAttrib = gl.getAttribLocation(shaderProgram, 'aJointWeight')
+
+gl.enableVertexAttribArray(vertexPosAttrib)
+gl.enableVertexAttribArray(vertexNormalAttrib)
+gl.enableVertexAttribArray(jointIndexAttrib)
 gl.enableVertexAttribArray(jointWeightAttrib)
 
 var ambientColorUni = gl.getUniformLocation(shaderProgram, 'uAmbientColor')
@@ -183,8 +180,8 @@ var directionalColorUni = gl.getUniformLocation(shaderProgram, 'uDirectionalColo
 var mVMatrixUni = gl.getUniformLocation(shaderProgram, 'uMVMatrix')
 var pMatrixUni = gl.getUniformLocation(shaderProgram, 'uPMatrix')
 
-var boneRotQuaternions = []
-var boneTransQuaternions = []
+var boneRotQuaternions = {}
+var boneTransQuaternions = {}
 for (var i = 0; i < 18; i++) {
   boneRotQuaternions[i] = gl.getUniformLocation(shaderProgram, `boneRotQuaternions[${i}]`)
   boneTransQuaternions[i] = gl.getUniformLocation(shaderProgram, `boneTransQuaternions[${i}]`)
@@ -194,13 +191,18 @@ var vertexPosBuffer = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexPosBuffer)
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(baseballPlayer.positions), gl.STATIC_DRAW)
 gl.vertexAttribPointer(vertexPosAttrib, 3, gl.FLOAT, false, 0, 0)
+var maxPosIndex = 0
+var maxFoo = 0
+baseballPlayer.positionIndices.forEach(function (index, foo) {
+  maxPosIndex = Math.max(maxPosIndex, index)
+  maxFoo = Math.max(maxFoo, foo)
+})
 
 var vertexNormalBuffer = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer)
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(baseballPlayer.normals), gl.STATIC_DRAW)
 gl.vertexAttribPointer(vertexNormalAttrib, 3, gl.FLOAT, false, 0, 0)
 
-console.log(baseballPlayer)
 // TODO: This is only 0-4 or so so only need a byte array
 // TODO: Rename to joint influencer buffer
 var jointIndexBuffer = gl.createBuffer()
@@ -221,11 +223,6 @@ gl.uniform3fv(ambientColorUni, [1, 0, 0])
 gl.uniform3fv(lightingDirectionUni, [1, -1, -1])
 gl.uniform3fv(directionalColorUni, [0, 0, 1])
 
-var modelMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, -25, 1]
-var viewMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
-var mVMatrix = glMat4.multiply([], viewMatrix, modelMatrix)
-
-gl.uniformMatrix4fv(mVMatrixUni, false, mVMatrix)
 gl.uniformMatrix4fv(pMatrixUni, false, glMat4.perspective([], Math.PI / 3, 1, 0.1, 100))
 
 var firstKeyframe = Object.keys(baseballPlayer.keyframes)[0]
@@ -237,10 +234,11 @@ baseballPlayer.keyframes = Object.keys(baseballPlayer.keyframes)
   }
   return dualQuats
 }, {})
-console.log(baseballPlayer.keyframes)
 
 var clockTime = 0
+// var yRotation = 0
 function draw () {
+  // yRotation += 0.02
   gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT)
 
   clockTime += 0.016
@@ -250,22 +248,24 @@ function draw () {
     jointNums: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 13, 14, 15, 16, 17],
     currentAnimation: {
       startTime: 0,
-      range: [0, 9]
+      range: [6, 17]
     }
   })
   for (var j = 0; j < 18; j++) {
     var rotQuat = animationData.joints[j].slice(0, 4)
     var transQuat = animationData.joints[j].slice(4, 8)
-    /*
-    rotQuat = baseballPlayer.keyframes[firstKeyframe][j].slice(0, 4)
-    transQuat = baseballPlayer.keyframes[firstKeyframe][j].slice(4, 8)
-    */
 
     gl.uniform4fv(boneRotQuaternions[j], rotQuat)
     gl.uniform4fv(boneTransQuaternions[j], transQuat)
   }
 
-  gl.drawElements(gl.TRIANGLES, baseballPlayer.positionIndices.length, gl.UNSIGNED_BYTE, 0)
+  var modelMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, -27, 1]
+  // glMat4.rotateY(modelMatrix, modelMatrix, yRotation)
+  var viewMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+  var mVMatrix = glMat4.multiply([], viewMatrix, modelMatrix)
+  gl.uniformMatrix4fv(mVMatrixUni, false, mVMatrix)
+
+  gl.drawElements(gl.TRIANGLES, baseballPlayer.positionIndices.length, gl.UNSIGNED_SHORT, 0)
 
   window.requestAnimationFrame(draw)
 }
